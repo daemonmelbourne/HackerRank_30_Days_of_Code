9499fce130c22a11c08ab51fdce83fe51af4465f
Pamela Pavlova <melbournedh@gmail.com>
2020-02-20 20:32:34 +0200

    Adding files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        int j = Convert.ToInt32(Console.ReadLine());
+        double e = Convert.ToDouble(Console.ReadLine());
+        string t = Console.ReadLine();
+
+        Console.WriteLine(i + j);
+        Console.WriteLine((e + d).ToString("F1"));
+        Console.WriteLine(s + t);
+    }
+}
